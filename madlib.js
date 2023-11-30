47386d3409c257d0ab6e67f794fe8c3e9c75ca7f
gorintlapavanprasad <132552737+gorintlapavanprasad@users.noreply.github.com>
2023-11-29 17:00:18 -0700

    Add files via upload

---
+++ madlib.js
@@ -0,0 +1,51 @@
+// madlib.js
+
+function generateStory() {
+  var adjective1 = document.getElementById("adjective1").value;
+  var pluralNoun1 = document.getElementById("pluralnoun1").value;
+  var destination1 = document.getElementById("destination1").value;
+  var adjective2 = document.getElementById("adjective2").value;
+  var transportation1 = document.getElementById("transportation1").value;
+  var adjective3 = document.getElementById("adjective3").value;
+  var activity1 = document.getElementById("activity1").value;
+  var number1 = document.getElementById("number1").value;
+  var adjective4 = document.getElementById("adjective4").value;
+
+  var story = `Once upon a time, in a land of ${adjective1} ${pluralNoun1}, there was a traveler heading to ${destination1}. The journey was ${adjective2}, and they decided to travel by ${transportation1}. Along the way, they encountered ${adjective3} ${activity1} happening in ${number1} different places. It was truly ${adjective4}!`;
+
+  document.getElementById("textarea").innerText = story;
+}
+// madlib.js
+
+function generateStory() {
+  var adjective1 = document.getElementById("adjective1").value;
+  var pluralNoun1 = document.getElementById("pluralnoun1").value;
+  var destination1 = document.getElementById("destination1").value;
+  var adjective2 = document.getElementById("adjective2").value;
+  var transportation1 = document.getElementById("transportation1").value;
+  var adjective3 = document.getElementById("adjective3").value;
+  var activity1 = document.getElementById("activity1").value;
+  var number1 = document.getElementById("number1").value;
+  var adjective4 = document.getElementById("adjective4").value;
+
+  var story = `Once upon a time, in a land of ${adjective1} ${pluralNoun1}, there was a traveler heading to ${destination1}. The journey was ${adjective2}, and they decided to travel by ${transportation1}. Along the way, they encountered ${adjective3} ${activity1} happening in ${number1} different places. It was truly ${adjective4}!`;
+
+  document.getElementById("textarea").innerText = story;
+}
+// madlib.js
+
+function generateStory() {
+  var adjective1 = document.getElementById("adjective1").value;
+  var pluralNoun1 = document.getElementById("pluralnoun1").value;
+  var destination1 = document.getElementById("destination1").value;
+  var adjective2 = document.getElementById("adjective2").value;
+  var transportation1 = document.getElementById("transportation1").value;
+  var adjective3 = document.getElementById("adjective3").value;
+  var activity1 = document.getElementById("activity1").value;
+  var number1 = document.getElementById("number1").value;
+  var adjective4 = document.getElementById("adjective4").value;
+
+  var story = `Once upon a time, in a land of ${adjective1} ${pluralNoun1}, there was a traveler heading to ${destination1}. The journey was ${adjective2}, and they decided to travel by ${transportation1}. Along the way, they encountered ${adjective3} ${activity1} happening in ${number1} different places. It was truly ${adjective4}!`;
+
+  document.getElementById("textarea").innerText = story;
+}
